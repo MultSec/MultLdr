6085ded7da844e7ac6e7a3b0bcaeb848dbfdd4f4
pengrey <rodrigoflima@ua.pt>
2024-08-21 14:35:59 +0100

    Added AES

--- test/main.py
+++ test/main.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
                     
                 ],
                 "payload_mods": [
-                    "/payload_mods/encryption/BFDecryption"
+                    "/payload_mods/encryption/AES"
                 ],
                 "post_comp": [
                     
